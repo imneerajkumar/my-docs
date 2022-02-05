ebe6523424e73c9eb786569bd6fa35533a59e8b4
Neeraj Kumar <nijjukr@gmail.com>
2022-02-05 09:19:39 +0530

    Deployed

---
+++ client/src/toolbar.js
@@ -0,0 +1,13 @@
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        
+  ['blockquote', 'code-block'],
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],                                       
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  [{ 'color': [] }, { 'background': [] }],          
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+  ['clean']                                         
+];
+
+export default toolbarOptions;
